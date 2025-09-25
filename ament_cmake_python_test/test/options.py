b103bd85238aedbd5df0d1c59d920d015e59ea64
R Kent James <kent@caspia.com>
2025-09-25 13:29:28 -0700

    Again adding lint-like fixes
    Signed-off-by: R Kent James <kent@caspia.com>

--- ament_cmake_python_test/test/options.py
+++ ament_cmake_python_test/test/options.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Options used to generate test packages from a template.
-"""
+"""Options used to generate test packages from a template."""
 
 # Set True to generate both a python package and a msg package for each python package
 # This is useful to test that python and msg packages can coexist in the same package
@@ -25,87 +23,85 @@ COMBINE_PYTHON_WITH_MSG = True
 TEST_ORDERING_SENSITIVITY = False
 
 DEFAULT_OPTIONS = {
-  'name': 'SET_ME',
-  'version': None,
-  'description': 'SET_ME',
-  'setup_cfg': None,
-  'destination': None,
-  'symlink_install': False,
-  'python_subdir': None,
-  'has_python': True,
-  'has_python_before': False, # This will only make sense when both python and msg are in the same package
-  'has_msg': False,
-  'scripts_destination': None,
+    'name': 'SET_ME',
+    'version': None,
+    'description': 'SET_ME',
+    'setup_cfg': None,
+    'destination': None,
+    'symlink_install': False,
+    'python_subdir': None,
+    'has_python': True,
+    'has_python_before': False,  # if both python and msg are in the same package
+    'has_msg': False,
+    'scripts_destination': None,
 }
 
 TESTS_OPTIONS = [
-  {
-    'name': 'python_package',
-    'description': 'Package with python code',
-  },
-  {
-    'name': 'msg_package',
-    'description': 'Package with only msg files',
-    'has_msg': True,
-    'has_python': False,
-  },
-  {
-    'name': 'python_package_symlink',
-    'description': 'Package with python code, installed with symlink in build',
-    'symlink_install': True,
-  },
-  {
-    'name': 'python_package_rename',
-    'description': 'Package with python code, installed from alternate directory name',
-    'python_subdir': 'renamed_dir',
-  },
-  {
-    'name': 'python_package_version',
-    'description': 'Package with python code, specifying version in ament_python_install_package',
-    'version': '6.7.89',
-  },
-  {
-    'name': 'python_package_setup',
-    'description': 'Package with python code, using setup.cfg for metadata',
-    'setup_cfg': 'config/setup.cfg',
-  },
-  {
-    'name': 'python_package_destination',
-    'description': 'Package with python code, installed to alternate destination',
-    'destination': 'new_destination',
-  },
-  {
-    'name': 'python_package_with_scripts',
-    'description': 'Package with python code',
-    'scripts_destination': 'lib/python_package_with_scripts',
-  },
+    {
+        'name': 'python_package',
+        'description': 'Package with python code',
+    },
+    {
+        'name': 'msg_package',
+        'description': 'Package with only msg files',
+        'has_msg': True,
+        'has_python': False,
+    },
+    {
+        'name': 'python_package_symlink',
+        'description': 'Package with python code, installed with symlink in build',
+        'symlink_install': True,
+    },
+    {
+        'name': 'python_package_rename',
+        'description': 'Package with python code, installed from alternate directory name',
+        'python_subdir': 'renamed_dir',
+    },
+    {
+        'name': 'python_package_version',
+        'description': 'Package with python code, specifying version',
+        'version': '6.7.89',
+    },
+    {
+        'name': 'python_package_setup',
+        'description': 'Package with python code, using setup.cfg for metadata',
+        'setup_cfg': 'config/setup.cfg',
+    },
+    {
+        'name': 'python_package_destination',
+        'description': 'Package with python code, installed to alternate destination',
+        'destination': 'new_destination',
+    },
+    {
+        'name': 'python_package_with_scripts',
+        'description': 'Package with python code',
+        'scripts_destination': 'lib/python_package_with_scripts',
+    },
 ]
 
 
 def get_options():
-  """
-  # return a list of options dictionaries for generating test packages.
-  """
-  tests_options = []
-  for options in TESTS_OPTIONS:
-    options = DEFAULT_OPTIONS | options
-    tests_options.append(options)
+    """Return a list of options dictionaries for generating test packages."""
+    tests_options = []
+    for options in TESTS_OPTIONS:
+        options = DEFAULT_OPTIONS | options
+        tests_options.append(options)
 
-    if COMBINE_PYTHON_WITH_MSG:
-      if options['name'].startswith('python_package'):
-        msg_options = options.copy()
-        msg_options['name'] += '_with_msg'
-        msg_options['description'] += 'and msg files'
-        msg_options['has_msg'] = True
-        tests_options.append(msg_options)
+        if COMBINE_PYTHON_WITH_MSG:
+            if options['name'].startswith('python_package'):
+                msg_options = options.copy()
+                msg_options['name'] += '_with_msg'
+                msg_options['description'] += 'and msg files'
+                msg_options['has_msg'] = True
+                tests_options.append(msg_options)
 
-      # This only makes sense when both python and msg are in the same package
-      if TEST_ORDERING_SENSITIVITY:
-        if options['name'].startswith('msg_package'):
-          py_options = options.copy()
-          py_options['name'] += '_with_python_before'
-          py_options['description'] += ' and python code before msg'
-          py_options['has_python_before'] = True
-          tests_options.append(py_options)
-    
-  return tests_options
+            # This only makes sense when both python and msg are in the same package
+            if TEST_ORDERING_SENSITIVITY:
+                if options['name'].startswith('msg_package'):
+                    py_options = options.copy()
+                    py_options['name'] += '_with_python_before'
+                    py_options['description'] += ' and python code before msg'
+                    py_options['has_python_before'] = True
+                    tests_options.append(py_options)
+
+    return tests_options
